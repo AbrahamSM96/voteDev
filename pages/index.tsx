59ca418ede3fbfd5a8bb0079a8a35e52780d3425
Abraham Serrano <abrahamserrano96@hotmail.com>
2022-01-11 10:47:45 -0600

    feat: feat components imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,14 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-import { useSession, signIn, signOut } from "next-auth/react";
-import { LoginButton } from "components/LoginButton";
+import { Header } from "components/Header";
 
 const Home: NextPage = () => {
-  const { data: session } = useSession();
-  console.log({ session });
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user?.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    );
-  }
   return (
     <>
-      <LoginButton />
+      <Head>
+        <title>vote-dev - Vota a los mejores jugadores del año</title>
+      </Head>
+      <Header />
     </>
   );
 };
