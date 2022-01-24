58b43ab9baab4c6d88e84abe869597aa09ab63bd
Abraham Serrano <abrahamserrano96@hotmail.com>
2022-01-24 01:09:47 -0600

    add(lib): add carpet lib with Prisma config

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { prisma } from "prisma/index";
+import prisma from "lib/prisma";
 
 export default NextAuth({
   // Configure one or more authentication providers
